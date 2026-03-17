9a86b69820f532a5aae4704134b3884b6c461bf8
hmm0411 <23520964@gm.uit.edu.vn>
2026-03-17 20:55:16 +0700

    fix

--- rl_engine/agent.py
+++ rl_engine/agent.py
@@ -1,9 +1,9 @@
 # thêm agent.py để định nghĩa agent (DQN, PPO, v.v.) sẽ học từ môi trường SDNEnv và digital twin
 import random
-
+from rl_engine.config import STATE_DIM, ACTION_DIM
 
 class DQNAgent:
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, STATE_DIM, ACTION_DIM):
         pass
 
     def select_action(self, state):
@@ -13,7 +13,7 @@ class DQNAgent:
         pass
 
 class PPOAgent:
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, STATE_DIM, ACTION_DIM):
         pass
 
     def select_action(self, state):
