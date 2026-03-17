55f03d47796d7e099cc6114140726a01b7982375
hmm0411 <23520964@gm.uit.edu.vn>
2026-03-17 20:05:51 +0700

    Fix(controller): init + env update

--- rl_engine/env.py
+++ rl_engine/env.py
@@ -17,8 +17,8 @@ from rl_engine.config import STATE_DIM, ACTION_DIM
 
 class SDNEnv(gym.Env):
 
-    def __init__(self):
-        self.controller = ControllerClient()
+    def __init__(self, controller_url="http://controller:8080"):
+        self.controller = ControllerClient(controller_url=controller_url)
         self.state_builder = StateBuilder()
         self.reward_calc = Reward()
 
