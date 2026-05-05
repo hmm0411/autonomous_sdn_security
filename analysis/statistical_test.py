a0eaccba7b73df6f3b4083dfb84b503973949623
hmm0411 <23520964@gm.uit.edu.vn>
2026-05-05 23:49:48 +0700

    fix path

--- analysis/statistical_test.py
+++ analysis/statistical_test.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from scipy.stats import ttest_ind
 
-df = pd.read_csv("results/evaluation_summary.csv")
-
-dqn = df[df.model == "DQN"]["mean_reward"]
-ppo = df[df.model == "PPO"]["mean_reward"]
+dqn = pd.read_csv("runs/models/dqn_metrics_by_seed.csv")["final_reward"]
+ppo = pd.read_csv("runs/models/ppo_metrics_by_seed.csv")["final_reward"]
 
 t, p = ttest_ind(dqn, ppo)
 
