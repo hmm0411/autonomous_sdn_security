e8225ba9d0513841b6b38d27e4302d11272fb0b6
hmm0411 <23520964@gm.uit.edu.vn>
2026-05-20 23:42:27 +0700

    fix training pipeline for logging

--- control_loop/serving.py
+++ control_loop/serving.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, jsonify
+import mlflow
 import mlflow.pytorch
 import torch
 import joblib
+import os
+import time
 from prometheus_client import start_http_server, Gauge
 from mlops.mlflow_manager import get_best_model
+
+# ===== CONFIG =====
+mlflow.set_tracking_uri(
+    os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000")
+)
 
 model = None
 scaler = None
@@ -16,19 +24,37 @@ model_used_g = Gauge("model_used", "Model selected", ["model"])
 def load_models():
     global model, scaler
 
-    best_model, _ = get_best_model()
-    model_uri = f"models:/{best_model.name}/{best_model.version}"
+    for _ in range(10):
+        try:
+            best_model, _ = get_best_model()
 
-    model = mlflow.pytorch.load_model(model_uri)
-    scaler = joblib.load("models/scaler.pkl")
+            model_uri = f"models:/{best_model.name}/Production"
+            model = mlflow.pytorch.load_model(model_uri)
 
-    print(f"[+] Loaded model: {model_uri}")
+            scaler = joblib.load("models/scaler.pkl")
+
+            print(f"[+] Loaded model: {model_uri}")
+            return
+
+        except Exception as e:
+            print("Retry loading model...", e)
+            time.sleep(5)
+
+    raise Exception("Cannot load model")
+
+# ===== HEALTH =====
+@app.route("/health", methods=["GET"])
+def health():
+    return {"status": "ok"}
 
 # ===== PREDICT =====
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         state = request.json["state"]
+
+        if len(state) != 9:
+            return jsonify({"error": "invalid state size"})
 
         state = scaler.transform([state])
         state = torch.tensor(state).float()
@@ -51,7 +77,7 @@ def predict():
 @app.route("/reload", methods=["POST"])
 def reload_model():
     load_models()
-    return jsonify({"status": "reloaded"})
+    return {"status": "reloaded"}
 
 # ===== RUN =====
 if __name__ == "__main__":
