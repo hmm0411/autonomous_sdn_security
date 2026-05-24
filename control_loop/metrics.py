ba5dd8aabb0784a4ffc09f8d70fb7226c3307847
hmm0411 <23520964@gm.uit.edu.vn>
2026-05-25 05:50:36 +0700

    fix pipeline

--- control_loop/metrics.py
+++ control_loop/metrics.py
@@ -9,20 +9,20 @@ action_g = Gauge('rl_action', 'Action', ['model_type'])
 cpu_g = Gauge('rl_controller_cpu', 'Controller CPU', ['model_type'])
 queue_g = Gauge('rl_queue_length', 'Queue length', ['model_type'])
 
-model_selected_g = Gauge('rl_model_selected', 'Selected model', ['model'])
+model_selected_g = Gauge('rl_model_selected', 'Selected model', ['model_type'])
 
 start_http_server(9100)
 
-def update_metrics(state, reward_prod, reward_staging, model_type, action):
-    reward_g.labels(model=model_type, stage='production').set(reward_prod)
-    reward_g.labels(model=model_type, stage='staging').set(reward_staging)
+def update_metrics(state, reward_prod, reward_staging, model_type_str, action):
+    # Đã sửa sạch sẽ thành model_type=...
+    reward_g.labels(model_type=model_type_str, stage='production').set(reward_prod)
+    reward_g.labels(model_type=model_type_str, stage='staging').set(reward_staging)
 
-    latency_g.labels(model=model_type).set(state[5]) # Chuẩn 8-dim: latency index 5
-    packet_loss_g.labels(model=model_type).set(state[6]) # Chuẩn 8-dim: loss index 6
-    flow_g.labels(model=model_type).set(state[2])
+    latency_g.labels(model_type=model_type_str).set(state[5]) 
+    packet_loss_g.labels(model_type=model_type_str).set(state[6]) 
+    flow_g.labels(model_type=model_type_str).set(state[2])
 
-    action_g.labels(model=model_type).set(action)
-    cpu_g.labels(model=model_type).set(state[7])
-    # queue_g.labels(model=model_type).set(state[6])
+    action_g.labels(model_type=model_type_str).set(action)
+    cpu_g.labels(model_type=model_type_str).set(state[7])
 
-    model_selected_g.labels(model=model_type).set(1)
+    model_selected_g.labels(model_type=model_type_str).set(1)
