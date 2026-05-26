04412851b8c74a6ed37847f8498e20d179fee448
hmm0411 <23520964@gm.uit.edu.vn>
2026-05-27 03:06:31 +0700

    fix pipeline of system

--- init_model.py
+++ init_model.py
@@ -7,8 +7,12 @@ from rl_engine.config import STATE_DIM, ACTION_DIM
 from dotenv import load_dotenv
 
 load_dotenv()  # Load biến môi trường từ .env
-mlflow.set_tracking_uri("http://localhost:30007") # Dùng port NodePort
+mlflow.set_tracking_uri("http://35.240.135.171:30007") # Dùng port NodePort
 mlflow.set_experiment("SDN_Autonomous_Security")
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://35.240.135.171:30005"
+os.environ["AWS_ACCESS_KEY_ID"] = "minioadmin"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "minioadmin"
 
 def init():
     agent = DQNAgent(STATE_DIM, ACTION_DIM)
